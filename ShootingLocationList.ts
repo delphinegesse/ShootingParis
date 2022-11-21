d236f12442e1b5afa49559473e2e4e0da93cc45f
simon <simon.ducros@etu.emse.fr>
2022-11-21 17:40:44 +0100

    ShootingLocationList.ts created

---
+++ ShootingLocationList.ts
@@ -0,0 +1,32 @@
+import {ShootingLocation} from "./ShootingLocation";
+
+export class ShootingLocationList {
+    listShootingLocation: ShootingLocation[] = []
+
+    addShootingLocation(shootingLocation: ShootingLocation): void {
+        if (!this.listShootingLocation.find((element: ShootingLocation) => element.title)) {
+            this.listShootingLocation.push(shootingLocation)
+        } // TITLE ???
+    }
+
+    getShootingLocation(name: string): ShootingLocation {
+        return <ShootingLocation>this.listShootingLocation.find(shootingLocation => shootingLocation.title === name);
+    }
+
+    getShootingLocationAt(address: string): ShootingLocation[] {
+        return this.getAllShootingLocations().filter(book => book.address === address);
+    }//d'une adresse
+
+    getShootingLocationFrom(title: string): ShootingLocation[] {
+        return this.getAllShootingLocations().filter(book => book.title === title);
+    }//d'un film
+
+    getAllShootingLocations(): ShootingLocation[] {
+        return this.listShootingLocation.sort((a:ShootingLocation , b:ShootingLocation) => {
+            return a.title.localeCompare(b.title);});
+    }
+
+    getTotalNumberOfBooks(): number {
+        return this.listShootingLocation.length;
+    }
+}
