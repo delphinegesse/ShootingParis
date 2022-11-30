21417b00e54c97e669bf747d3a11cc08b6e16b7d
simon <simon.ducros@etu.emse.fr>
2022-11-30 19:07:05 +0100

    Remarque main

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(ShootingLocationModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT;
-  await app.listen(port);
+  await app.listen(port); //ou est ce que tu définis port ?? perso dans mon code j'avais mis 3000, et dans postman aussi du coup c'etait localhost:3000
 }
 bootstrap();
